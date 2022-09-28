f74310d688ae4d7ad896f5c198087a4383a76993
Lets Shop-Git <abhishek.dasgupta@indusnet.co.in>
2022-09-28 14:46:01 +0530

    Terminal executable files & Browser Compatibility implementation

--- src/main/java/util/Base.java
+++ src/main/java/util/Base.java
@@ -5,12 +5,13 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -25,7 +26,9 @@ public class Base {
 		Properties prop=new Properties();
 		FileInputStream fileInputStream=new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\resources\\GlobalData.properties");
 		prop.load(fileInputStream);
-		String browserName=prop.getProperty("browser");
+		//Ternary operator to check browser from terminal(maven property) if there not then proceed from here Global property
+		String browserName=System.getProperty("browser")!=null ? System.getProperty("browser"): prop.getProperty("browser");
+		//prop.getProperty("browser");
 		
 		if(browserName.equalsIgnoreCase("chrome")) 
 		{
@@ -36,10 +39,15 @@ public class Base {
 		else if(browserName.equalsIgnoreCase("firefox"))
 		{
 			//Code for firefox browser
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"\\driver\\geckodriver.exe");
+			driver=new FirefoxDriver();
 		}
-		else if(browserName.equalsIgnoreCase("ie"))
+		else if(browserName.equalsIgnoreCase("edge"))
 		{
 			//Code for IE browser
+			System.setProperty("webdriver.edge.driver", System.getProperty("user.dir")+"\\driver\\latestedgedriver.exe");
+			driver=new EdgeDriver();
+			
 		}
 		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
